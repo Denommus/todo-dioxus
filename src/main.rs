bb47e19368acba075fc333e4828153daafd36bf4
Yuri Albuquerque <yuridenommus@gmail.com>
2024-07-21 16:01:45 -0300

    Todo app

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@
 use dioxus::prelude::*;
 use tracing::Level;
 
+
+#[derive(Clone, Debug, PartialEq, Eq, Props)]
+struct TodoItem {
+    contents: String,
+    done: bool,
+}
+
 #[derive(Clone, Routable, Debug, PartialEq)]
 enum Route {
     #[route("/")]
     Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
 }
 
 fn main() {
@@ -28,28 +33,75 @@ fn App() -> Element {
 }
 
 #[component]
-fn Blog(id: i32) -> Element {
+fn TodoList(items: Signal<Vec<TodoItem>>) -> Element {
+    let rendered_items =
+        items.iter().zip(0..).map(|(item, i)| {
+            let style = if item.done {
+                r#"
+                  text-decoration: line-through
+                "#
+            } else {
+                ""
+            };
+            rsx! {
+                li {
+                    style: style,
+                    {item.contents.clone()}
+                    input {
+                        r#type: "checkbox",
+                        checked: item.done,
+                        onchange: move |_| {
+                            let mut writer = items.write();
+                            writer[i].done = !writer[i].done;
+                        }
+                    }
+                }
+            }
+        });
+
     rsx! {
-        Link { to: Route::Home {}, "Go to counter" }
-        "Blog post {id}"
+        ul {
+            {rendered_items}
+            button {
+                onclick: move |_| {
+                    items.set(items.iter().filter_map(|x| if x.done {
+                        None
+                    } else {
+                        Some(TodoItem {
+                            contents: x.contents.clone(),
+                            done: false,
+                        })
+                    }).collect())
+                },
+                "Clear"
+            }
+        }
     }
 }
 
 #[component]
 fn Home() -> Element {
-    let mut count = use_signal(|| 0);
+    let mut input = use_signal(|| "".to_string());
+    let mut items: Signal<Vec<TodoItem>> = use_signal(|| vec![]);
 
     rsx! {
-        Link {
-            to: Route::Blog {
-                id: count()
-            },
-            "Go to blog"
-        }
         div {
-            h1 { "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
+            h1 { "Todo App" }
+            input {
+                value: "{input}",
+                oninput: move |event| input.set(event.value())
+            }
+            button {
+                onclick: move |_| {
+                    items.write().push(TodoItem {
+                        contents: input.read().clone(),
+                        done: false,
+                    });
+                    input.set("".to_string())
+                },
+                "Add"
+            }
+            TodoList { items: items }
         }
     }
 }
